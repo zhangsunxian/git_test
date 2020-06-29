c1b23e847c554b36a8c41bc684a940420a221be8
zhangsunxian <18796287856@163.com>
2020-06-29 16:44:50 +0800

    添加了注释

--- gittest/src/com/cumt/git/FirstClass.java
+++ gittest/src/com/cumt/git/FirstClass.java
@@ -3,7 +3,7 @@ package com.cumt.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First Class");
-    }
+    }//main方法
     public void test(){
         return;
     }
